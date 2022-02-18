2925954797f0f10bf872c6e5029469c1f9bb8986
FilipBianga <fbianga94@gmail.com>
2022-02-18 15:30:33 +0100

    Hidden events but not deleted

--- config/calendar.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-$headings = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
--- views/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-<div id="footer">
-    <p>2021 © Bianga</p>
-    <a href="http://biblioteka.gumed.edu.pl/" target="_blank">Biblioteka Główna GUMed</a>
-</div>
-
-</body>
-</html>
